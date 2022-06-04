cc235f586ff344ca5f3813e1747288234a6647f5
Ivan-H-C <ivanhc.pp@gmail.com>
2022-06-04 11:30:43 -0500

    decouple activity page from bloc provider

--- lib/activity/view/activity_page.dart
+++ lib/activity/view/activity_page.dart
@@ -9,15 +9,18 @@ import 'package:loader_overlay/loader_overlay.dart';
 class ActivityPage extends StatelessWidget {
   const ActivityPage({
     Key? key,
-    required this.initialActivity,
     this.isDialog = false,
   }) : super(key: key);
 
   static GoRoute route() {
     return GoRoute(
       path: 'activity',
-      builder: (context, state) => ActivityPage(
-        initialActivity: state.extra! as Activity,
+      builder: (context, state) => BlocProvider(
+        create: (context) => ActivityBloc(
+          activitiesRepository: context.read<ActivitiesRepository>(),
+          initialActivity: state.extra! as Activity,
+        ),
+        child: const ActivityPage(),
       ),
     );
   }
@@ -30,51 +33,49 @@ class ActivityPage extends StatelessWidget {
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
         ),
-        child: ActivityPage(
-          initialActivity: activity,
-          isDialog: true,
+        child: BlocProvider(
+          create: (context) => ActivityBloc(
+            activitiesRepository: context.read<ActivitiesRepository>(),
+            initialActivity: activity,
+          ),
+          child: const ActivityPage(
+            isDialog: true,
+          ),
         ),
       ),
     );
   }
 
-  final Activity initialActivity;
   final bool isDialog;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ActivityBloc(
-        activitiesRepository: context.read<ActivitiesRepository>(),
-        initialActivity: initialActivity,
-      ),
-      child: LoaderOverlay(
-        child: BlocListener<ActivityBloc, ActivityState>(
-          listenWhen: (previous, current) => previous.status != current.status,
-          listener: (context, state) {
-            switch (state.status) {
-              case ActivityStatus.initial:
-                break;
-              case ActivityStatus.loading:
-                context.loaderOverlay.show();
-                break;
-              case ActivityStatus.success:
-                context.loaderOverlay.hide();
-                Navigator.of(context).pop();
-                break;
-              case ActivityStatus.failure:
-                context.loaderOverlay.hide();
-                // TODO(ivan): Handle ActivityStatus.failure.
-                break;
-            }
-          },
-          child: ActivityLayoutBuilder(
-            headerButtons: (_) => ActivityHeaderButtons(isDialog: isDialog),
-            nameTextField: (_) => const ActivityNameTextField(),
-            descriptionTextField: (_) => const ActivityDescriptionTextField(),
-            datePicker: (_) => ActivityDatePicker(isDialog: isDialog),
-            timePickers: (_) => const ActivityTimePickers(),
-          ),
+    return LoaderOverlay(
+      child: BlocListener<ActivityBloc, ActivityState>(
+        listenWhen: (previous, current) => previous.status != current.status,
+        listener: (context, state) {
+          switch (state.status) {
+            case ActivityStatus.initial:
+              break;
+            case ActivityStatus.loading:
+              context.loaderOverlay.show();
+              break;
+            case ActivityStatus.success:
+              context.loaderOverlay.hide();
+              Navigator.of(context).pop();
+              break;
+            case ActivityStatus.failure:
+              context.loaderOverlay.hide();
+              // TODO(ivan): Handle ActivityStatus.failure.
+              break;
+          }
+        },
+        child: ActivityLayoutBuilder(
+          headerButtons: (_) => ActivityHeaderButtons(isDialog: isDialog),
+          nameTextField: (_) => const ActivityNameTextField(),
+          descriptionTextField: (_) => const ActivityDescriptionTextField(),
+          datePicker: (_) => ActivityDatePicker(isDialog: isDialog),
+          timePickers: (_) => const ActivityTimePickers(),
         ),
       ),
     );
