17f9ec9ef447ed2e2d9921ffbaa5c80191ef6b65
Ivan-H-C <ivanhc.pp@gmail.com>
2022-06-16 14:08:11 -0500

    test(schedule): add widgets tests

---
+++ test/schedule/widgets/schedule_timetable_test.dart
@@ -0,0 +1,72 @@
+import 'package:dynamic_timeline/dynamic_timeline.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_planner/schedule/schedule.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:routines_api/routines_api.dart';
+
+import '../../helpers/helpers.dart';
+import '../schedule_mocks.dart';
+
+void main() {
+  group('ScheduleTimetable', () {
+    late ScheduleBloc scheduleBloc;
+
+    final mockRoutines = [
+      Routine(
+        userID: 'userID',
+        id: 1,
+        name: 'routine 1',
+        day: 1,
+        startTime: DateTime(1970, 1, 1, 8),
+        endTime: DateTime(1970, 1, 1, 10),
+      ),
+      Routine(
+        userID: 'userID',
+        id: 2,
+        name: 'routine 2',
+        day: 3,
+        startTime: DateTime(1970, 1, 1, 10),
+        endTime: DateTime(1970, 1, 1, 12),
+      ),
+      Routine(
+        userID: 'userID',
+        id: 3,
+        name: 'routine 3',
+        day: 2,
+        startTime: DateTime(1970, 1, 1, 9),
+        endTime: DateTime(1970, 1, 1, 11),
+      ),
+    ];
+
+    setUp(() {
+      scheduleBloc = MockScheduleBloc();
+
+      when(() => scheduleBloc.state).thenReturn(
+        ScheduleState(
+          routines: mockRoutines,
+        ),
+      );
+    });
+
+    Widget buildSubject({
+      ScheduleSize currentSize = ScheduleSize.small,
+    }) {
+      return BlocProvider.value(
+        value: scheduleBloc,
+        child: ScheduleTimetable(
+          currentSize: currentSize,
+        ),
+      );
+    }
+
+    testWidgets('renders DynamicTimeline with RoutineCards', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      expect(find.byType(DynamicTimeline), findsOneWidget);
+      expect(find.byType(RoutineCard), findsNWidgets(3));
+    });
+
+  });
+}
