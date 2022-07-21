df8a01683fd5a1c2786304d58c5cb868dcfb391c
Ivan-H-C <ivanhc.pp@gmail.com>
2022-07-21 11:05:44 -0500

    test(task): add widgets tests

---
+++ test/task/widgets/task_check_box_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planner/task/task.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../helpers/helpers.dart';
+import '../task_mocks.dart';
+
+void main() {
+  group('TaskCheckBox', () {
+    late TaskBloc taskBloc;
+
+    setUp(() {
+      taskBloc = MockTaskBloc();
+
+      when(() => taskBloc.state).thenReturn(
+        TaskState(initialTask: mockTask),
+      );
+    });
+
+    Widget buildSubject() {
+      return BlocProvider.value(
+        value: taskBloc,
+        child: const TaskCheckBox(),
+      );
+    }
+
+    testWidgets('renders CheckBox', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      expect(find.byType(Checkbox), findsOneWidget);
+    });
+
+    testWidgets(
+        'add TaskCompletetionToggled '
+        'to TaskBloc when is pressed', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      await tester.tap(find.byType(Checkbox));
+
+      verify(
+        () => taskBloc.add(const TaskCompletetionToggled()),
+      ).called(1);
+    });
+  });
+}
---
+++ test/task/widgets/task_text_field_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planner/planner/bloc/planner_bloc.dart';
+import 'package:flutter_planner/task/task.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../helpers/helpers.dart';
+import '../../planner/planner_mocks.dart';
+import '../task_mocks.dart';
+
+void main() {
+  group('TaskTextField', () {
+    late TaskBloc taskBloc;
+    late PlannerBloc plannerBloc;
+
+    setUp(() {
+      taskBloc = MockTaskBloc();
+      plannerBloc = MockPlannerBloc();
+
+      when(() => taskBloc.state).thenReturn(
+        TaskState(initialTask: mockTask),
+      );
+    });
+
+    Widget buildSubject({
+      String initialTitle = '',
+    }) {
+      return MultiBlocProvider(
+        providers: [
+          BlocProvider.value(value: taskBloc),
+          BlocProvider.value(value: plannerBloc),
+        ],
+        child: TaskTextField(initialTitle: initialTitle),
+      );
+    }
+
+    testWidgets('renders a TextField', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      expect(find.byType(TextField), findsOneWidget);
+    });
+
+    testWidgets(
+        'add TaskTitleChanged '
+        'to TaskBloc when new value is entered', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      await tester.enterText(find.byType(TextField), 'new title');
+
+      verify(
+        () => taskBloc.add(const TaskTitleChanged('new title')),
+      ).called(1);
+    });
+
+    group('onEditingComplete', () {
+      testWidgets(
+          'add PlannerNewTaskAdded '
+          'to PlannerBloc when title is not empty', (tester) async {
+        await tester.pumpApp(buildSubject());
+
+        await tester.enterText(find.byType(TextField), 'new title');
+
+        tester.testTextInput.closeConnection();
+
+        verify(
+          () => plannerBloc.add(const PlannerNewTaskAdded()),
+        ).called(1);
+      });
+
+      testWidgets('unfocus when title is empty', (tester) async {
+        await tester.pumpApp(buildSubject(initialTitle: 'title'));
+
+        await tester.enterText(find.byType(TextField), '');
+
+        tester.testTextInput.closeConnection();
+
+        await tester.pump();
+
+        final focusNode =
+            (tester.widget(find.byType(TextField)) as TextField).focusNode;
+
+        expect(focusNode?.hasFocus, false);
+      });
+    });
+  });
+}
