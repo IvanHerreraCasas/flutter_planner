7ae48c56585109fd0b61b7fa5982f7b886e85d1c
Ivan-H-C <ivanhc.pp@gmail.com>
2022-07-23 15:09:35 -0500

    chore: remove unused authentication ui pkg

--- packages/authentication/authentication_ui/lib/authentication_ui.dart
+++
@@ -1,7 +0,0 @@
-library authentication_ui;
-
-export 'src/authentication.dart';
-export 'src/builders/builders.dart';
-export 'src/models/models.dart';
-export 'src/sign_in.dart';
-export 'src/sign_up.dart';
--- packages/authentication/authentication_ui/lib/src/builders/builders.dart
+++
@@ -1,5 +0,0 @@
-export 'body_builder.dart';
-export 'button_builder.dart';
-export 'error_builder.dart';
-export 'header_builder.dart';
-export 'helper_builder.dart';
