1ea710acf05bb04aad7a7a78d95181961bd269d3
Ivan-H-C <ivanhc.pp@gmail.com>
2022-07-23 19:28:39 -0500

    test(app_event): test abstract app event

--- test/app/bloc/app_event_test.dart
+++ test/app/bloc/app_event_test.dart
@@ -3,8 +3,19 @@
 import 'package:flutter_planner/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class MockAppEvent extends AppEvent {}
+
 void main() {
   group('AppEvent', () {
+    group('BaseAppEvent', () {
+      test('supports value equality', () {
+        expect(MockAppEvent(), equals(MockAppEvent()));
+      });
+
+      test('props are correct', () {
+        expect(MockAppEvent().props, equals(<Object?>[]));
+      });
+    });
     group('AppRouteChanged', () {
       test('supports value equality', () {
         expect(AppRouteChanged('/home'), equals(AppRouteChanged('/home')));
