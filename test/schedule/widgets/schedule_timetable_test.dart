ef48733b380d6166129ec798d12795622eaf463f
Ivan-H-C <ivanhc.pp@gmail.com>
2022-07-23 19:28:54 -0500

    test(schedule_timetable): test time changes

--- test/schedule/widgets/schedule_timetable_test.dart
+++ test/schedule/widgets/schedule_timetable_test.dart
@@ -66,5 +66,57 @@ void main() {
       expect(find.byType(DynamicTimeline), findsOneWidget);
       expect(find.byType(RoutineCard), findsNWidgets(3));
     });
+
+    testWidgets(
+        'add ScheduleRoutineChanged '
+        'to ScheduleBloc when the top is dragged', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      final centerLocation = tester.getCenter(
+        find.byKey(ValueKey(mockRoutines[0])),
+      );
+
+      final topLocation = centerLocation.translate(0, -50);
+
+      await tester.dragFrom(topLocation, const Offset(0, -25));
+
+      // interval extent is 50 pixels and intervalDuration is 1 hour
+      // => 25 pixels = 30 min
+      verify(
+        () => scheduleBloc.add(
+          ScheduleRoutineChanged(
+            mockRoutines[0].copyWith(
+              startTime: DateTime(1970, 1, 1, 7, 30),
+            ),
+          ),
+        ),
+      ).called(1);
+    });
+
+    testWidgets(
+        'add ScheduleRoutineChanged '
+        'to ScheduleBloc when the bottom is dragged', (tester) async {
+      await tester.pumpApp(buildSubject());
+
+      final centerLocation = tester.getCenter(
+        find.byKey(ValueKey(mockRoutines[0])),
+      );
+      // interval extent is 50 pixels and intervalDuration is 1 hour
+      // mockRoutines[0] has a duration of 2 hour, the half is 50 pixels
+      final bottomLocation = centerLocation.translate(0, 49);
+
+      await tester.dragFrom(bottomLocation, const Offset(0, 25));
+
+      // 25 pixels = 30 min
+      verify(
+        () => scheduleBloc.add(
+          ScheduleRoutineChanged(
+            mockRoutines[0].copyWith(
+              endTime: DateTime(1970, 1, 1, 10, 30),
+            ),
+          ),
+        ),
+      ).called(1);
+    });
   });
 }
