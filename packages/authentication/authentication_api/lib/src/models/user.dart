63ed1d0f66528ca624d4891ff58b1e7628aa7492
Ivan-H-C <ivanhc.pp@gmail.com>
2022-07-26 18:32:22 -0500

    feat(auth): add name and isEditable props

--- packages/authentication/authentication_api/lib/src/models/user.dart
+++ packages/authentication/authentication_api/lib/src/models/user.dart
@@ -7,15 +7,28 @@ class User extends Equatable {
   /// {@macro user}
   const User({
     required this.id,
+    this.name,
     this.email,
+    this.isEditable = true,
   });
 
   /// The users id
   final String id;
 
+  /// The users name
+  final String? name;
+
   /// The users emails address
   final String? email;
 
+  /// Determines if the user can be edited
+  final bool isEditable;
+
   @override
-  List<Object?> get props => [id, email];
+  List<Object?> get props => [
+        id,
+        name,
+        email,
+        isEditable,
+      ];
 }
