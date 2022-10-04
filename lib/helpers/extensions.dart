dcdb4a19d21e9a24c1b763c25531445e9e84287a
Ivan-H-C <ivanhc.pp@gmail.com>
2022-10-04 12:25:53 -0500

    feat(extensions): dateTime copyWith

--- lib/helpers/extensions.dart
+++ lib/helpers/extensions.dart
@@ -25,3 +25,39 @@ extension DateTimeJson on DateTime {
     );
   }
 }
+
+extension DateTimeCopyWith on DateTime {
+  DateTime copyWith({
+    int? year,
+    int? month,
+    int? day,
+    int? hour,
+    int? minute,
+    int? second,
+    int? millisecond,
+    int? microsecond,
+  }) {
+    if (isUtc) {
+      return DateTime.utc(
+        year ?? this.year,
+        month ?? this.month,
+        day ?? this.day,
+        hour ?? this.hour,
+        minute ?? this.minute,
+        second ?? this.second,
+        millisecond ?? this.millisecond,
+        microsecond ?? this.microsecond,
+      );
+    }
+    return DateTime(
+      year ?? this.year,
+      month ?? this.month,
+      day ?? this.day,
+      hour ?? this.hour,
+      minute ?? this.minute,
+      second ?? this.second,
+      millisecond ?? this.millisecond,
+      microsecond ?? this.microsecond,
+    );
+  }
+}
